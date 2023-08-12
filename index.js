8fe37ec8b0123dd70508df117d9a49d11ed5709e
Arifa1015 <ajjiarifa1514@gmail.com>
2023-08-12 16:10:09 +0530

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function btn1Function() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0"
+}
+
+function btn2Function() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97"
+}
+
+function btn3Function() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2"
+}
+
+function btn4Function() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9"
+}
